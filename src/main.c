c765fd937c03f41bcaa7b92f26445a3ed42caf88
MaxKhyz <maxmailbox2017@gmail.com>
2022-11-13 16:48:50 +0200

    Modified source code of stack with fix size to var size

--- src/main.c
+++ src/main.c
@@ -11,8 +11,8 @@
 #include"queue.h"
 
  /******************************** Definition **********************************/
-#define FIX_LENGTH_STACK_TEST   (0)
-#define FIX_LENGTH_QUEUE_TEST   (1)
+#define VAR_LENGTH_STACK_TEST   (1)
+#define VAR_LENGTH_QUEUE_TEST   (0)
 #define ADDITIONAL_TEST         (1)
 
 #define DISCARD_RETURN(f) ((void)f)
@@ -22,83 +22,84 @@
     printf("***************************************************************\n")
 /********************************* Entry point ********************************/
 int main() {
-#if FIX_LENGTH_STACK_TEST
-    TEST_START("variable size stack");
-    if (create_stack(STACK_LENGTH)) {
-        printf("\nAdded symbols with ASCII codes from 48 to 58 into a stack\n");
-        for (uint8_t i = 0; i < (STACK_LENGTH + 1); i++) {
-            DISCARD_RETURN(push_stack(48 + i));
-        }
+#if VAR_LENGTH_STACK_TEST
+	TEST_START("variable size stack");
+	if (create_stack(STACK_LENGTH)) {
+		printf("\nAdded symbols with ASCII codes from 48 to 58 into a stack\n");
+		for (uint8_t i = 0; i < (STACK_LENGTH + 1); i++) {
+			DISCARD_RETURN(push_stack(48 + i));
+		}
 
 #if ADDITIONAL_TEST
-        printf("\nGet three characters from the stack:\n");
-        for (uint8_t i = 0; i < 3; i++) {
-            printf("%c\n", pop_stack());
-        }
-        printf("Added new symbols with ASCII codes from 65 to 67\n");
-        DISCARD_RETURN(push_stack(65));
-        DISCARD_RETURN(push_stack(66));
-        DISCARD_RETURN(push_stack(67));
+		printf("\nGet three characters from the stack:\n");
+		for (uint8_t i = 0; i < 3; i++) {
+			printf("%c\n", pop_stack());
+		}
+		printf("Added new symbols with ASCII codes from 65 to 67\n");
+		DISCARD_RETURN(push_stack(65));
+		DISCARD_RETURN(push_stack(66));
+		DISCARD_RETURN(push_stack(67));
 
-        printf("\nThe content of the variable-length stack:\n");
-        for (uint8_t i = 0; i < STACK_LENGTH + 1; i++) {
-            printf("%c\n", pop_stack());
-        }
-        printf("Added new symbols with ASCII codes from 68 to 70\n");
-        DISCARD_RETURN(push_stack(68));
-        DISCARD_RETURN(push_stack(69));
-        DISCARD_RETURN(push_stack(70));
+		printf("\nThe content of the variable-length stack:\n");
+		for (uint8_t i = 0; i < STACK_LENGTH + 1; i++) {
+			printf("%c\n", pop_stack());
+		}
+		printf("Added new symbols with ASCII codes from 68 to 70\n");
+		DISCARD_RETURN(push_stack(68));
+		DISCARD_RETURN(push_stack(69));
+		DISCARD_RETURN(push_stack(70));
 #endif
 
-        printf("The top of variable-length stack: %c\n", top_stack());
-        printf("The content of variable-length stack:\n");
-        while (!is_empty_stack()) {
-            printf("%c\n", pop_stack());
-        }
+		printf("The top of variable-length stack: %c\n", top_stack());
+		printf("The content of variable-length stack:\n");
+		while (!is_empty_stack()) {
+			printf("%c\n", pop_stack());
+		}
 
-        destroy_stack();
-    }
-    TEST_FINISH;
+		destroy_stack();
+	}
+	TEST_FINISH;
 #endif
-    /*----------------------------------------------------------------------------*/
-#if FIX_LENGTH_QUEUE_TEST
-    TEST_START("variable size queue");
-    if (create_queue(QUEUE_LENGTH)) {
-        printf("\nAdded symbols with ASCII codes from 48 to 58 into a queue\n");
-        for (uint8_t i = 0; i < (QUEUE_LENGTH + 1); i++) {
-            DISCARD_RETURN(push_queue(48 + i));
-        }
+	/*----------------------------------------------------------------------------*/
+#if VAR_LENGTH_QUEUE_TEST
+	TEST_START("variable size queue");
+	if (create_queue(STACK_LENGTH)) {
+		printf("\nAdded symbols with ASCII codes from 48 to 58 into a queue\n");
+		for (uint8_t i = 0; i < (QUEUE_LENGTH + 1); i++) {
+			DISCARD_RETURN(push_queue(48 + i));
+		}
 
 #if ADDITIONAL_TEST
-        printf("\nGet three characters from the queue:\n");
-        for (uint8_t i = 0; i < 3; i++) {
-            printf("%c\n", pop_queue());
-        }
-        printf("Added new symbols with ASCII codes from 65 to 67\n");
-        DISCARD_RETURN(push_queue(65));
-        DISCARD_RETURN(push_queue(66));
-        DISCARD_RETURN(push_queue(67));
+		printf("\nGet three characters from the queue:\n");
+		for (uint8_t i = 0; i < 3; i++) {
+			printf("%c\n", pop_queue());
+		}
+		printf("Added new symbols with ASCII codes from 65 to 67\n");
+		DISCARD_RETURN(push_queue(65));
+		DISCARD_RETURN(push_queue(66));
+		DISCARD_RETURN(push_queue(67));
 
-        printf("\nThe content of variable-length queue:\n");
-        for (uint8_t i = 0; i < QUEUE_LENGTH + 1; i++) {
-            printf("%c\n", pop_queue());
-        }
-        printf("Added new symbols with ASCII codes from 68 to 70\n");
-        DISCARD_RETURN(push_queue(68));
-        DISCARD_RETURN(push_queue(69));
-        DISCARD_RETURN(push_queue(70));
+		printf("\nThe content of variable-length queue:\n");
+		for (uint8_t i = 0; i < STACK_LENGTH + 1; i++) {
+			printf("%c\n", pop_queue());
+		}
+		printf("Added new symbols with ASCII codes from 68 to 70\n");
+		DISCARD_RETURN(push_queue(68));
+		DISCARD_RETURN(push_queue(69));
+		DISCARD_RETURN(push_queue(70));
 #endif
 
-        printf("The front of variable-length queue: %c\n", front_queue());
-        printf("The content of variable-length queue:\n");
-        while (!is_empty_queue()) {
-            printf("%c\n", pop_queue());
-        }
-        destroy_queue();
-    }
-    TEST_FINISH;
+		printf("The front of variable-length queue: %c\n", front_queue());
+		printf("The content of variable-length queue:\n");
+		while (!is_empty_queue()) {
+			printf("%c\n", pop_queue());
+		}
+		destroy_queue();
+	}
+	TEST_FINISH;
 #endif
-    /*----------------------------------------------------------------------------*/
-    getchar();
-    return 0;
+	/*----------------------------------------------------------------------------*/
+	getchar();
+	return 0;
 }
+
--- src/stack.c
+++ src/stack.c
@@ -1,86 +1,89 @@
 /**
  * @file    stack.c
- * @version 1.3.0
- * @authors Anton Chernov
- * @date    23/02/2022
- * @date    14/10/2022
+ * @version 
+ * @authors 
+ * @date    12/11/2022
+ * @date    12/11/2022
  */
+
 
 /****************************** Included files ********************************/
 #include "common.h"
 #include <stdlib.h>
+#include <malloc.h>
 /****************************** Private  variables ****************************/
-static data_t stack[STACK_LENGTH];
+static data_t * stack;
+static uint8_t s_size;
 static volatile uint8_t top;
 static volatile uint8_t count;
-static uint8_t _is_create;
 /****************************** Private  functions ****************************/
 static bool stack_check(void) {
 	bool result = true;
-	if (!_is_create) {
+	if (!stack) {
 		printf("Memory access error\n");
 		result = false;
 	}
 	return result;
 }
-
-
 /********************* Application Programming Interface **********************/
-void create_stack(void) {
-        count = 0u;
-        top = 0u;
-        _is_create = true;
+bool create_stack(uint8_t buf_size) {
+	bool result = false;
+	stack = (data_t*)calloc(buf_size, sizeof(data_t));
+	if (stack_check()) {
+		s_size = buf_size;
+		count = 0u;
+		top = 0u;
+		result = true;
+	}
+	return result;
 }
 /*----------------------------------------------------------------------------*/
 bool push_stack(uint8_t item) {
-    bool result = false;
-    if (stack_check()) {
-        if (top < (STACK_LENGTH - 1u)) {
-            if (count++){
-				stack[++top] = item;
-			}
-            else { 
-				stack[top] = item;
-			}
-            result = true;
-        }
-        else { printf("Buffer overflow\n"); }
-    }
-    return result;
+	bool result = false;
+	if (stack_check()) {
+		if (top < s_size - 1u) {
+			if (count++) { stack[++top] = item; }
+			else { stack[top] = item; }
+			result = true;
+		}
+		else { printf("Buffer overflow\n"); }
+	}
+	return result;
 }
 /*----------------------------------------------------------------------------*/
 uint8_t pop_stack(void) {
-    uint8_t result = 0u;
-    if (stack_check()) {
-        if (count) {
-            if (!top) { result = stack[top]; }
-            else { result = stack[top--]; }
-            count--;
-        }
-        else { printf("Stack is empty\n"); }
-    }
-    return result;
+	uint8_t result = 0u;
+	if (stack_check()) {
+		if (count) {
+			if (!top) { result = stack[top]; }
+			else { result = stack[top--]; }
+			count--;
+		}
+		else { printf("Stack is empty\n"); }
+	}
+	return result;
 }
 /*----------------------------------------------------------------------------*/
 uint8_t top_stack(void) {
-    uint8_t result = 0u;
-    if (stack_check()) {
-        if (count) {
-            result = stack[top];
-        }
-    }
-    return result;
+	uint8_t result = 0u;
+	if (stack_check()) {
+		if (count) {
+			result = stack[top];
+		}
+	}
+	return result;
 }
 /*----------------------------------------------------------------------------*/
 bool is_empty_stack(void) {
-    bool result = 0u;
-    if (stack_check()) {
-        result = (count == 0u);
-    }
-    return result;
+	bool result = 0u;
+	if (stack_check()) {
+		result = (count == 0u);
+	}
+	return result;
 }
 /*----------------------------------------------------------------------------*/
 void destroy_stack(void) {
-	_is_create = false;
+	free(stack);
+	stack = NULL;
 }
 /******************************************************************************/
